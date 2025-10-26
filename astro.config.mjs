0421d5626257c90bebbb5e6e7adc1174f0f85100
DJAYI <danilo.arenasyi@gmail.com>
2025-10-26 13:03:00 -0500

    fix: Update dependencies for @astrojs/vercel and add @vercel/analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
